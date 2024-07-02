888f8e5f136dfb63dd4d5ea71b6c3b52bdb208ed
Abdullah Al Masrur <abdullahalmasrur8@gmail.com>
2024-07-03 02:47:42 +0600

    Add files via upload

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -70,7 +70,13 @@ function Menu() {
   return (
     <menu className="menu">
       <h2>Our Menu</h2>
-      <Pizza
+
+      <ul className="pizzas">
+        {pizzaData.map((pizza) => (
+          <Pizza pizzaObj={pizza} key={pizza.name} />
+        ))}
+      </ul>
+      {/* <Pizza
         name="Pizza Spinaci"
         ingredients="Tomato, Spinach"
         photoName="pizzas/spinaci.jpg"
@@ -81,7 +87,7 @@ function Menu() {
         ingredients="Bun, Petty"
         photoName="pizzas/funghi.jpg"
         price={25}
-      />
+      /> */}
     </menu>
   );
 }
@@ -89,14 +95,14 @@ function Menu() {
 function Pizza(props) {
   console.log(props);
   return (
-    <div className="pizza">
-      <img src={props.photoName} alt={props.name} />
+    <li className="pizza">
+      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
       <div>
-        <h3>{props.name}</h3>
-        <p>{props.ingredients}</p>
-        <span>{props.price + 3}</span>
+        <h3>{props.pizzaObj.name}</h3>
+        <p>{props.pizzaObj.ingredients}</p>
+        <span>{props.pizzaObj.price + 3}</span>
       </div>
-    </div>
+    </li>
   );
 }
 
