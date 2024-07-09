92167b348a8fa08c5bd70ed5f1df989ebe735b23
John Doe <abdullahalmasrur8@gmail.com>
2024-07-09 15:17:04 +0600

    #

--- 11-use-popcorn/src/Components/Nav.js
+++ 11-use-popcorn/src/Components/Nav.js
@@ -1,17 +1,42 @@
 import { useState } from "react";
-import { Search } from "./Search.js";
 
 export function Nav() {
   return (
     <nav className="nav-bar">
-      <div className="logo">
-        <span role="img">🍿</span>
-        <h1>usePopcorn</h1>
-      </div>
+      <Logo />
       <Search />
-      <p className="num-results">
-        Found <strong>X</strong> results
-      </p>
+      <NumResult />
     </nav>
+  );
+}
+
+function Logo() {
+  return (
+    <div className="logo">
+      <span role="img">🍿</span>
+      <h1>usePopcorn</h1>
+    </div>
+  );
+}
+
+function NumResult() {
+  return (
+    <p className="num-results">
+      Found <strong>X</strong> results
+    </p>
+  );
+}
+
+function Search() {
+  const [query, setQuery] = useState("");
+
+  return (
+    <input
+      className="search"
+      type="text"
+      placeholder="Search movies..."
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+    />
   );
 }
