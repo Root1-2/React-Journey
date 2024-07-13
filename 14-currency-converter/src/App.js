08fef68b0499294bff52218ae5c302454cb96d6b
John Doe <abdullahalmasrur8@gmail.com>
2024-07-13 22:25:49 +0600

    #

--- 14-currency-converter/src/App.js
+++ 14-currency-converter/src/App.js
@@ -1,3 +1,58 @@
+import { useState, useEffect } from "react";
+
 export default function App() {
-  return <div>Meow</div>;
+  const [amount, setAmount] = useState(1);
+  const [fromCur, setFromCur] = useState("EUR");
+  const [toCur, setToCur] = useState("USD");
+  const [result, setResult] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(
+    function () {
+      async function convert() {
+        setIsLoading(true);
+        const res = await fetch(
+          `https://api.frankfurter.app/latest?amount=${amount}&from=${fromCur}&to=${toCur}`
+        );
+        const data = await res.json();
+        setResult(data.rates[toCur]);
+        setIsLoading(false);
+      }
+
+      if (fromCur === toCur) return setResult(amount);
+      convert();
+    },
+    [amount, fromCur, toCur]
+  );
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={amount}
+        onChange={(e) => setAmount(Number(e.target.value))}
+        disabled={isLoading}
+      />
+      <select
+        value={fromCur}
+        onChange={(e) => setFromCur(e.target.value)}
+        disabled={isLoading}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">IND</option>
+      </select>
+      <select onChange={(e) => setToCur(e.target.value)} disabled={isLoading}>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+
+      <p>
+        {result} {toCur}
+      </p>
+    </div>
+  );
 }
