ed6f92bcc5162453d5c798eaa4829ffc2670cb28
John Doe <abdullahalmasrur8@gmail.com>
2024-07-13 23:58:39 +0600

    using refs

--- 11-use-popcorn/src/Components/Nav.js
+++ 11-use-popcorn/src/Components/Nav.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 export function Nav({ children }) {
   return <nav className="nav-bar">{children}</nav>;
@@ -22,9 +22,27 @@ export function NumResult({ movies }) {
 }
 
 export function Search({ query, setQuery }) {
+  // useEffect(function () {
+  //   const el = document.querySelector(".search");
+  //   el.focus();
+  // }, []);
+
+  const inputElement = useRef(null);
+
   useEffect(function () {
-    const el = document.querySelector(".search");
-    el.focus();
+    function callback(e) {
+      if (document.activeElement === inputElement.current) {
+        return;
+      }
+
+      if (e.code === "Enter") {
+        inputElement.current.focus();
+        setQuery("");
+      }
+    }
+
+    document.addEventListener("keydown", callback);
+    return () => document.addEventListener("keydown", callback);
   }, []);
 
   return (
@@ -34,6 +52,7 @@ export function Search({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputElement}
     />
   );
 }
