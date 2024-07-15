3ff10022e2192ceffe93365c118e5d25d1ef6a5c
John Doe <abdullahalmasrur8@gmail.com>
2024-07-15 21:40:20 +0600

    use custom API and useReducer

--- 15-react-quiz/src/App.js
+++ 15-react-quiz/src/App.js
@@ -1,9 +1,48 @@
-import DateCounter from "./DateCounter";
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+
+const initialState = {
+  questions: [],
+  // "Loading", "Error", "Error", "Active", "Finished"
+  status: "Loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.paylaod,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "Error",
+      };
+    default:
+      throw new Error("Action Unknown");
+  }
+}
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 }
