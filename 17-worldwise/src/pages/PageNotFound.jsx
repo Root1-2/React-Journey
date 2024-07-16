00a770fe50e69a552b4baeaf336566e4161fa389
John Doe <abdullahalmasrur8@gmail.com>
2024-07-16 23:23:44 +0600

    #

--- 17-worldwise/src/pages/PageNotFound.jsx
+++ 17-worldwise/src/pages/PageNotFound.jsx
@@ -1,12 +1,7 @@
-import PageNav from "../components/PageNav";
-
-function PageNotFound() {
+export default function PageNotFound() {
   return (
     <div>
-      <PageNav />
-      <p>Not Found Here</p>
+      <h1>Page not found 😢</h1>
     </div>
   );
 }
-
-export default PageNotFound;
