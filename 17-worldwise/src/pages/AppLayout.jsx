42fb56e40e8f9bf2e3c804cba200ec8dded14154
John Doe <abdullahalmasrur8@gmail.com>
2024-07-16 23:50:05 +0600

    appLayout

--- 17-worldwise/src/pages/AppLayout.jsx
+++ 17-worldwise/src/pages/AppLayout.jsx
@@ -1,5 +1,15 @@
+import Map from "../components/Map";
+import SideBar from "../components/SideBar";
+
+import styles from "./AppLayout.module.css";
+
 function AppLayout() {
-  return <div></div>;
+  return (
+    <div className={styles.app}>
+      <SideBar />
+      <Map />
+    </div>
+  );
 }
 
 export default AppLayout;
