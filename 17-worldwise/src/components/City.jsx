303c5a99983ec21305b3a0c4b793f9addb4b899e
John Doe <abdullahalmasrur8@gmail.com>
2024-07-17 02:26:57 +0600

    queryString

--- 17-worldwise/src/components/City.jsx
+++ 17-worldwise/src/components/City.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 import styles from "./City.module.css";
 
 const formatDate = (date) =>
@@ -10,7 +10,13 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function City() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
   const { id } = useParams();
+
   // TEMP DATA
   const currentCity = {
     cityName: "Lisbon",
