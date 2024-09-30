d6ccac3a6d347627b125387c11699d8b40d71ba0
John Doe <abdullahalmasrur8@gmail.com>
2024-10-01 02:13:00 +0600

    font import

--- 21-the-wild-oasis/app/layout.js
+++ 21-the-wild-oasis/app/layout.js
@@ -1,6 +1,12 @@
 import Logo from "./_components/Logo";
 import Navigation from "./_components/navigation";
 import "@/app/_styles/globals.css";
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata = {
   // title: "The Wild Oasis",
@@ -15,7 +21,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-100 min-h-screen">
+      <body
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+      >
         <header>
           <Logo />
         </header>
