d6caf68997c01809be32f091bab8ec4a62a11ceb
John Doe <abdullahalmasrur8@gmail.com>
2024-10-01 02:23:37 +0600

    optimizing image w/ Image comp

--- 21-the-wild-oasis/app/layout.js
+++ 21-the-wild-oasis/app/layout.js
@@ -1,5 +1,5 @@
+import Header from "./_components/Header";
 import Logo from "./_components/Logo";
-import Navigation from "./_components/navigation";
 import "@/app/_styles/globals.css";
 import { Josefin_Sans } from "next/font/google";
 
@@ -22,14 +22,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
       >
-        <header>
-          <Logo />
-        </header>
-        <Navigation />
-        <main>{children}</main>
-        <footer className="text-center">Copyright by The Wild Oasis</footer>
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
