0679ce38779e8c2b72e805b3a3af5f27d0560aae
John Doe <abdullahalmasrur8@gmail.com>
2024-10-02 00:59:21 +0600

    data fetching and displaying

--- 21-the-wild-oasis/app/cabins/page.js
+++ 21-the-wild-oasis/app/cabins/page.js
@@ -1,13 +1,13 @@
 import CabinCard from "@/app/_components/CabinCard";
 import Counter from "@/app/_components/Counter";
+import { getCabins } from "../_lib/data-service";
 
 export const metadata = {
   title: "Cabins",
 };
 
-export default function Page() {
-  // CHANGE
-  const cabins = [];
+export default async function Page() {
+  const cabins = await getCabins();
 
   return (
     <div>
